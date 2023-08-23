6e0b9687f2e07efc9d88ddee4d3c536a469797e0
salim.unlu <salim.unlu@bilgeadam.com>
2023-08-23 12:27:59 +0300

    app.py dosyasında uygulamanın kullanıcıdan metin girişi alıp bu metni hemen bir etikette görüntülemesini sağlayan daha karmaşık bir arayüz oluşturuldu.

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
-# PyQt6 kütüphanesinden gerekli modüllerin ithal edilmesi
-from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
+
 import sys
 
 
-# QMainWindow sınıfından türetilen özelleştirilmiş ana pencere sınıfı
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()  # Üst sınıfın başlatıcı metodunun çağrılması
+        super().__init__()
 
-        self.setWindowTitle("My App")  # Pencere başlığının ayarlanması
-        button = QPushButton("Press Me!")  # Buton oluşturulması
-        self.setMinimumSize(QSize(300, 300))  # Pencere minimum boyutunun ayarlanması
+        self.setWindowTitle("My App")
 
-        # Pencerenin merkezi bileşeni olarak butonun ayarlanması
-        self.setCentralWidget(button)
+        self.label = QLabel()
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(container)
 
 
-# QApplication nesnesinin oluşturulması
 app = QApplication(sys.argv)
 
-# Özelleştirilmiş MainWindow sınıfından bir nesne oluşturulması
 window = MainWindow()
-window.show()  # Pencerenin gösterilmesi
+window.show()
 
-# Uygulamanın ana döngüsünün başlatılması
 app.exec()
